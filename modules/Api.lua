3a61c925aed444de2ef25be27665f63cb32108ec
DocsFoxr <alexr.05@outlook.com>
2022-06-14 00:56:57 -0500

    Updated 2.2

--- modules/Api.lua
+++ modules/Api.lua
@@ -13,7 +13,7 @@ function Api:FetchAsync(url, method)
 
         if request.StatusCode == 200 then
             function Proto.toJson()
-                return { StatusCode = request.StatusCode, Response = request, Data = HttpService:JSONDecode(request.Body), Url = url, Method = method}
+                return { StatusCode = request.StatusCode, Response = request, Data = HttpService:JSONDecode(request.Body), Url = url, Method = method }
             end
         end
 
@@ -27,7 +27,23 @@ function Api:GetHashCode()
 end
 
 function Api:GetVersion()
+    local function Hash_Rounded(sha, values)
+        local counter = 0
+        local Shart_Hash = ""
 
+        if values > string.len(sha) then
+           return print("El value pasado excede la cantidad de valores del sha dado.") 
+        end
+
+        Shart_Hash = sha:gsub("%x", function(n)
+            if counter <= values then counter += 1 return n else return "" end
+        end)
+
+        if Shart_Hash == nil then return "Not Found Version..." else return Shart_Hash end
+    end
+
+    local response = Api:FetchAsync("https://api.github.com/repos/DocsFoxr/Aeis-Hub-Library/commits/main").toJson()
+    return Hash_Rounded(response.Data.sha, 6)
 end
 
 return Api
